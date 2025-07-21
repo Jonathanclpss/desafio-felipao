a668e4238bf932f90b4c47dc2eb392d9cfd3314a
Jonathanclpss <jonathan2609pedrozo@gmail.com>
2025-07-21 18:51:33 -0300

    desafio do Felipe

--- src/index.js
+++ src/index.js
@@ -1,2 +1,24 @@
-let nome = "Jonathan"
-console.log(nome)
+let nomeHeroi = "Bilu ";
+let expericeiencia = 0;
+let nivel = ""
+
+if (expericeiencia <= 1000){
+    nivel = "Ferro";
+} else if (expericeiencia >= 1001 && expericeiencia <=2000){
+    nivel = "Bronze";
+} else if (expericeiencia >=2001 && expericeiencia <=5000){
+    nivel = "Prata";
+} else if (expericeiencia >=5001 && expericeiencia <=7000){
+    nivel = "Ouro";
+} else if (exoericencia >=7001 && expericeiencia <=8000){
+    nivel = "Platina";
+} else if (expericeiencia >=8001 && expericeiencia <=9000){
+    nivel = "Ascendente";
+} else if (expericeiencia >= 9001 && expericeiencia <=10000){
+    nivel = 'Imortal';
+} else if (expericeiencia >= 10001){
+    nivel = "Radiante";
+}
+
+
+console.log("O heroi de nome: " + nomeHeroi + "esta no nivel " + nivel)
