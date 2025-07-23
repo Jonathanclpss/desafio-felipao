5f9350223786b43fc1a178b740b021b10ae04d58
Jonathanclpss <jonathan2609pedrozo@gmail.com>
2025-07-23 10:04:30 -0300

    desafio 2

---
+++ desafio2.js
@@ -0,0 +1,27 @@
+
+let calculosDaPartida = calcularSaldo (80, 10)
+let nivel = ""
+
+ if (calculosDaPartida <= 10 ) {
+        nivel = "Ferro";
+    } else if(calculosDaPartida >= 11 && calculosDaPartida <= 20){
+      nivel = "Bronze";
+} else if (calculosDaPartida >= 21 && calculosDaPartida <= 50){
+      nivel = "Prata";
+} else if(calculosDaPartida >= 51 && calculosDaPartida <= 80){
+        nivel = "Ouro";
+} else if (calculosDaPartida >= 81 && calculosDaPartida <= 90){
+        nivel = "Diamante";
+} else if (calculosDaPartida >= 91 && calculosDaPartida <=100){
+        nivel = "Lendário";
+}  else if (calculosDaPartida >= 101 ){
+        nivel = "Imortal";
+}
+
+
+console.log(`O heroi tem de ${calculosDaPartida} vitorias e esta no nivel ${nivel}`)
+
+function calcularSaldo(vitorias, derrotas){
+    let calculo = vitorias - derrotas
+    return calculo
+}
